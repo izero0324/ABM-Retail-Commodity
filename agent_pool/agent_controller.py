9cd750a9bed4d8e9b51914d802374379f58a2e86
izero0324 <andrew1997tw24@gmail.com>
2024-03-16 23:49:47 +0000

    rework agent controller

--- agent_pool/agent_controller.py
+++ agent_pool/agent_controller.py
@@ -5,49 +5,26 @@ def agent_choose(agent_name):
     '''
     A gateway to choose from different agents
     '''
+    is_buy = 'B' in agent_name
+
     if 'ZIP' in agent_name:
-        if 'B' in agent_name:
-            print("[Agent] I'm a ZIP buy agent")
-            ZIP_buy_agent(agent_name)
-        else:
-            print("[Agent] I'm a ZIP sell agent")
-            ZIP_sell_agent(agent_name)
+        ZIP_agent(agent_name, 10, is_buy=is_buy)
 
     elif 'ZI' in agent_name:
-        if 'B' in agent_name:
-            print("[Agent] I'm a ZI buy agent")
-            ZI_buy_agent(agent_name)
-        else:
-            print("[Agent] I'm a ZI sell agent")
-            ZI_sell_agent(agent_name)
+        ZI_agent(agent_name, is_buy=is_buy)
 
     elif 'MD' in agent_name:
-        if 'B' in agent_name:
-            print("[Agent] I'm a MD buy agent")
-            market_demand_buy_agent(agent_name)
-        else:
-            print("[Agent] I'm a MD sell agent")
-            market_demand_sell_agent(agent_name)
+        market_demand_agent(agent_name,is_buy=is_buy)
 
     elif 'PN' in agent_name:
-        if 'B' in agent_name:
-            print("[Agent] I'm a PN buy agent")
-            penetration_buy_agent(agent_name)
-        else:
-            print("[Agent] I'm a PN sell agent")
-            penetration_sell_agent(agent_name)
+        penetration_agent(agent_name,is_buy=is_buy)
     
     elif 'DYN' in agent_name:
         print("[Agent] I'm a DYN sell agent")
-        dynamic_sell_agent(agent_name)
+        dynamic_sell_agent(agent_name,is_buy=False)
     
     elif 'DT' in agent_name:
-        if 'B' in agent_name:
-            print("[Agent] I'm a DT buy agent")
-            decide_trend_buy_agent(agent_name)
-        else:
-            print("[Agent] I'm a DT sell agent")
-            decide_trend_sell_agent(agent_name)
+        decide_trend_agent(agent_name,is_buy=is_buy)
 
     else:
         print("[Agent] I'm a Random agent")
--- agent_pool/agents.py
+++ agent_pool/agents.py
@@ -3,68 +3,44 @@ from agent_pool.strategies import Strategies
 import random
 import numpy as np
 
-def ZI_buy_agent(agent_name):
+def ZI_agent(agent_name, is_buy: bool):
+    side = 'B' if is_buy else 'S'
     strat = Strategies()
-    strat.init(agent_name, 'B')
+    strat.init(agent_name, side)
     side, agent_p, agent_q =  strat.ZI()
+    post_order( 1, agent_p, agent_q, agent_name, side) 
+
+def ZIP_agent(agent_name, GreedyLevel, is_buy:bool ):
+    side = 'B' if is_buy else 'S'
+    strat = Strategies()
+    strat.init(agent_name, side)
+    side, agent_p, agent_q =  strat.ZIP(GreedyLevel)
     post_order( 1, agent_p, agent_q, agent_name, side) #post_order(1, price, quant,name, Buy/Sell)
 
-def ZI_sell_agent(agent_name):
+def market_demand_agent(agent_name, is_buy:bool ):
+    side = 'B' if is_buy else 'S'
     strat = Strategies()
-    strat.init(agent_name, 'S')
-    side, agent_p, agent_q =  strat.ZI()
-    post_order( 1, agent_p, agent_q, agent_name, side) #post_order(1, price, quant,name, Buy/Sell)
-
-def ZIP_buy_agent(agent_name):
-    strat = Strategies()
-    strat.init(agent_name, 'B')
-    side, agent_p, agent_q =  strat.ZIP(10)
-    post_order( 1, agent_p, agent_q, agent_name, side) #post_order(1, price, quant,name, Buy/Sell)
-
-def ZIP_sell_agent(agent_name):
-    strat = Strategies()
-    strat.init(agent_name, 'S')
-    side, agent_p, agent_q =  strat.ZIP(10)
-    post_order( 1, agent_p, agent_q, agent_name, side) #post_order(1, price, quant,name, Buy/Sell)
-
-def market_demand_buy_agent(agent_name):
-    strat = Strategies()
-    strat.init(agent_name,'B')
+    strat.init(agent_name, side)
     side, agent_p, agent_q  = strat.dynamic_pricing_strategy()
     post_order( 1, agent_p, agent_q, agent_name, side) 
 
-def market_demand_sell_agent(agent_name):
-    B1 = Strategies()
-    B1.init(agent_name,'S')
-    side, agent_p, agent_q  = B1.dynamic_pricing_strategy()
-    post_order( 1, agent_p, agent_q, agent_name, side) 
-
-def penetration_buy_agent(agent_name):
+def penetration_agent(agent_name,is_buy:bool ):
+    side = 'B' if is_buy else 'S'
     strat = Strategies()
-    strat.init(agent_name,'B')
+    strat.init(agent_name,side)
     side, agent_p, agent_q  = strat.penetration()
     post_order( 1, agent_p, agent_q, agent_name, side) 
 
-def penetration_sell_agent(agent_name):
+def dynamic_sell_agent(agent_name, is_buy: bool ):
+    if not is_buy: side='S'
     strat = Strategies()
-    strat.init(agent_name,'S')
-    side, agent_p, agent_q  = strat.penetration()
-    post_order( 1, agent_p, agent_q, agent_name, side) 
-
-def dynamic_sell_agent(agent_name):
-    strat = Strategies()
-    strat.init(agent_name,'S')
+    strat.init(agent_name,side)
     side, agent_p, agent_q  = strat.dynamic()
     post_order( 1, agent_p, agent_q, agent_name, side) 
 
-def decide_trend_buy_agent(agent_name):
+def decide_trend_agent(agent_name,is_buy:bool ):
+    side = 'B' if is_buy else 'S'
     strat = Strategies()
-    strat.init(agent_name,'B')
-    side, agent_p, agent_q  = strat.decide_order_trend_strategy()
-    post_order( 1, agent_p, agent_q, agent_name, side) 
-
-def decide_trend_sell_agent(agent_name):
-    strat = Strategies()
-    strat.init(agent_name,'S')
+    strat.init(agent_name,side)
     side, agent_p, agent_q  = strat.decide_order_trend_strategy()
     post_order( 1, agent_p, agent_q, agent_name, side) 
