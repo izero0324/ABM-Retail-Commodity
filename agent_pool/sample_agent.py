f116a6aef36a96b5302c0738ede3f01dfec3fb41
izero0324 <andrew1997tw24@gmail.com>
2024-03-17 19:02:37 +0000

    remove random_agent

--- agent_pool/sample_agent.py
+++ agent_pool/sample_agent.py
@@ -2,21 +2,6 @@ from tools.api_interface import post_order
 import random
 import numpy as np
 
-
-def random_agent(agent_name):
-    #historical_price(days, agent_name)
-    agent_num = 10
-    agent_pool = np.arange(0, agent_num, 1).tolist()
-    side = "B"
-    for agent in agent_pool:
-        agent_name = 'B000'+str(agent)
-        agent_q = random.randint(1, 10)
-        agent_p = random.randint(97, 103)
-        side_rnd = random.randint(0,1)
-        if side_rnd == 1:
-            side = "S"
-        post_order( 1, agent_p, agent_q, agent_name, side)
-
 def single_random_agent(agent_name):
     side = "B"
     agent_name = agent_name
