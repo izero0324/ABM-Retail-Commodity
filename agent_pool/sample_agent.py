372b91447658386f0cd65f4feea951c69a7a61a1
izero0324 <andrew1997tw24@gmail.com>
2024-03-17 19:10:42 +0000

    add mix strategy example

--- agent_pool/sample_agent.py
+++ agent_pool/sample_agent.py
@@ -1,8 +1,10 @@
-from tools.api_interface import post_order
 import random
 import numpy as np
 
-def single_random_agent(agent_name):
+from tools.api_interface import post_order
+from agent_pool.strategies import Strategies
+
+def single_random_agent(agent_name: str, is_buy: bool):
     side = "B"
     agent_name = agent_name
     agent_q = random.randint(1, 10)
@@ -11,3 +13,32 @@ def single_random_agent(agent_name):
     if side_rnd == 1:
         side = "S"
     post_order( 1, agent_p, agent_q, agent_name, side) #post_order(1, price, quant,name, Buy/Sell)
+
+def ZI_agent(agent_name, is_buy: bool):
+    # Decide if this agent is buyer/seller
+    side = 'B' if is_buy else 'S'
+    # Initial class `Strategies`
+    strat = Strategies()
+    strat.init(agent_name, side)
+    # Input your strategy here and get return of price and quantity
+    side, agent_p, agent_q =  strat.ZI()
+    post_order( 1, agent_p, agent_q, agent_name, side) # Post order
+
+def mix_strategy_agent_example(agent_name: str, is_buy: bool):
+    # Decide if this agent is buyer/seller
+    side = 'B' if is_buy else 'S'
+    # Initial class `Strategies`
+    strat = Strategies()
+    strat.init(agent_name, side)
+    # Input your strategy here and get return of price and quantity
+    side, agent_p1, agent_q1 =  strat.ZI()
+    side, agent_p2, agent_q2 =  strat.ZIP(10)
+    # Mix your strategy here
+    agent_price = np.mean(agent_p1,agent_p2) 
+    agent_quant = np.mean(agent_q1,agent_q2)
+    post_order( 1, agent_price, agent_quant, agent_name, side) # Post order
+
+
+
+
+
